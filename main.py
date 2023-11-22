29d6f0864cef63bd53356595ef4584021f4f6689
Geedhavarshini <geedhavarshinii@gmail.com>
2023-11-22 15:43:44 +0530

    converted to yaml file

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def histogram(num):
-    lst = list(map(int, str(num)))
-    for i in lst:
-        print(i*str(i))
-        
-num = input("Enter a number: ")
-histogram(num)
+import yaml
+
+if __name__ == '__main__':
+    #loading the yaml file
+    with open("histogram_code.yaml", "r") as file:
+        yaml_data = yaml.safe_load(file)
+    #executing the python code stored as 'code' in the yaml file
+    exec(yaml_data['code'])
