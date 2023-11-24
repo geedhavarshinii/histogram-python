de6a5c47c5e361826a83f430f18483c835ca78f2
Geedhavarshini <geedhavarshinii@gmail.com>
2023-11-24 11:42:13 +0530

    workflow setup

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ def histogram(num):
       # Convert given number to list of integers
       lst = list(map(int, str(num)))
       # Iterate through the list to print the number as a string
-      for i in lst:
+      for i in num:
           print(i * str(i))
 
-num = input("Enter a number: ")
+#num = input("Enter a number: ")
+num = 345
 histogram(num)
